89d2c451de7c35d18d6772ed9f27e8badb7f9ee6
unknown <gpolis@D20118.ad.lattelekom.biz>
2014-05-04 15:39:56 +0300

    Full role resolution with setToken. Returns promise of STS.

--- ngAws.js
+++ ngAws.js
@@ -10,16 +10,41 @@ angular.module('ngAws', [])
       RoleSessionName: 'web-identity-federation'
     };
     var aws = null;
+    var sts = null;
 
     // Private constructor
-    function AwsService() {
+    function AwsService($q) {
       aws = AWS;
+      sts = new AWS.STS();
+      var d = $q.defer();
 
       this.setToken = function(token, providerId) {
+
         if (providerId) {
           config.ProviderId = providerId;
         }
-        aws.config.credentials = new aws.WebIdentityCredentials(config);
+
+        var params = {
+          RoleArn: config.RoleArn,
+          RoleSessionName: config.RoleSessionName,
+          WebIdentityToken: token,
+          ProviderId: providerId,
+        };
+
+        sts.assumeRoleWithWebIdentity(params, function(err, data) {
+          if (err) {
+            // console.log(err, err.stack);
+            d.reject(err);
+          } else {
+            // console.log(data.Credentials);
+            aws.config.accessKeyId = data.Credentials.AccessKeyId;
+            aws.config.secretAccessKey = data.Credentials.SecretAccessKey;
+            aws.config.sessionToken = data.Credentials.SessionToken;
+            d.resolve(data);
+          }
+        });
+
+        return d.promise;
       };
       this.getAWS = function () {
         return aws;
@@ -32,7 +57,7 @@ angular.module('ngAws', [])
     };
 
     // Method for instantiating
-    this.$get = function () {
-      return new AwsService();
-    };
+    this.$get = ['$q', function ($q) {
+      return new AwsService($q);
+    }];
   });
